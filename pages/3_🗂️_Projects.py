4ab0d7cf82218ee9b8f3d5be5b6221557bcfab44
mayuras7685 <mayurasodara@gmail.com>
2023-04-21 22:16:39 +0530

    Add expander in project page

--- pages/3_🗂️_Projects.py
+++ pages/3_🗂️_Projects.py
@@ -2,3 +2,16 @@ import streamlit as st
 
 st.title('Projects')
 
+with st.container():
+  col1,col2 = st.columns((1, 2))
+  with col1:
+    st.image('https://devfolio-prod.s3.ap-south-1.amazonaws.com/hackathons/2cf7715148724d268e9e4b31d46c30ea/assets/favicon/512.jpeg')
+  with col2:
+    st.write("Project Title")
+    st.text("""
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx""")
+
+st.expander()
