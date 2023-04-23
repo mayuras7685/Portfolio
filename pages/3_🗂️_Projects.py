13f1c3e623fc577644a897706d6cba5763ca290e
mayuras7685 <mayurasodara@gmail.com>
2023-04-23 15:43:39 +0530

    Added Hackthon info.

--- pages/3_🗂️_Projects.py
+++ pages/3_🗂️_Projects.py
@@ -7,22 +7,20 @@ with st.container():
   with col1:
     st.image('https://devfolio-prod.s3.ap-south-1.amazonaws.com/hackathons/2cf7715148724d268e9e4b31d46c30ea/assets/favicon/512.jpeg')
   with col2:
-    st.write("Project Title")
-    st.text("""
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx""")
+    st.subheader("Olympic Data Analysis Web Application")
+    st.write("""
+    This analysis will provide detailed and accurate information regarding various factors which leads to the evolution of Olympic Games and improvement of Countries/Players over the time in visual format.
+    """)
+    st.markdown('`EDA`, `Streamlit`, `pandas`, `matplotlib`, `plotly`, `seaborn`')
+
+st.write('---')
 
 with st.container():
   col3,col4 = st.columns((1, 2))
   with col3:
     st.image('https://devfolio-prod.s3.ap-south-1.amazonaws.com/hackathons/2cf7715148724d268e9e4b31d46c30ea/assets/favicon/512.jpeg')
   with col4:
-    st.write("Project Title")
-    st.text("""
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx""")
+    st.header("Deadly Animal Detection")
+    st.write("""Custom object detection model that detects 6 classes (Tiger, Lion, Hyena, Cheetah, Wolf, Fox) and And came up with an Interface that detects Wild animals from webcam feed, images, and URLs of images""")
+    st.markdown('`Computer Vision`, `openncv-python`, `RCNN`, `Yolo`, `Roboflow`, `colab` ')
 
--- pages/4_🏆_Hackathons.py
+++ pages/4_🏆_Hackathons.py
@@ -3,10 +3,11 @@ from PIL import Image
 
  
 
-def txt(a, b, c, d, hed, img):
+def txt(a, b, c, d, name, hed, img):
    st.header(hed) #Header
    st.image(img) #Imgage 
    st.write(f'{a}') #Description
+   st.markdown(f'##### Project: `{name}`')
    st.markdown(f'##### {b}: {c}') #Skills explored during hackthon
    st.markdown(f'##### Project: {d}') #github link
 
@@ -23,51 +24,71 @@ st.title('Hackathons')
 tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["HackSVIT", "Hackout'22", "Hackthisfall 3.0", "DotSlash 6.0", "HackVengers", "WittyHacks 3.0"])
 
 with tab1:   
-   txt('My first hackthon and started journey that never ends, to attending   such hackathon we can up to date with latest technology advancemnets, thier in hackthon we creted sort of feedback form that calculates the Happiness index of school students.',
+   txt('I participated HackSVIT By SVIT, Vasad. it was my first hackthon and started journey that never ends, to attending   such hackathon we can up to date with latest technology advancemnets, thier in hackthon we creted sort of feedback form that calculates the Happiness index of school students.',
       'Skills', 
       '`React` , `Node.js`, `Mysql`, `Tailwind CSS`',
       'https://github.com/mayuras7685/SVIT-project-api',
+      'Happiness Index for Schools',
       'HackSVIT',
       h1)
 
   
 
 with tab2:
-   txt('My first hackthon and started journey that never ends, to attending   such hackathon we can up to date with latest technology advancemnets, thier in hackthon we creted sort of feedback form that calculates the Happiness index of school students.',
+   txt("""I participated in hackathon, Hackout'22 by Synapse at Dhirubhai Ambani Institute of Information and Communication Technology with friends as my teammates. Our project idea was a Happiness index for school with self assessment facilities. (in simple words mood tracking of students and show that in numbers and visuals).
+   winning over the hearts and minds of judges, and secured 4th spot on the leaderboard(Special mention award).
+""",
       'Skills', 
       '`HTML`, `CSS`, `Flask`, `Mongodb`, `Figma`',
       'https://github.com/mayuras7685/Geek-Unkils_Submission_H22',
+      'Happiness Index for Schools',
       "Hackot'22",
       h2)
 
 with tab3:
-   txt('My first hackthon and started journey that never ends, to attending   such hackathon we can up to date with latest technology advancemnets, thier in hackthon we creted sort of feedback form that calculates the Happiness index of school students.',
+   txt("""I participated in HackThisFall hackathon, a 36-hour long event     organised by MLH (Major League Hacking), a renowned international hackathon community. It was an incredible experience where I learned about several new technologies which are currently being used in industries. 
+
+   During the event I had the opportunity to explore the postmanapi platform which provides API services and learned about a similar platform Apyhub, also learned Head less CMS (Content Management System) By StoryBlock. 
+   Additionally, I gained some valuable insights into  git / github. """,
       'Skills', 
       '`React`, `Tailwind CSS`, `Flask`, `OCR`, `TensorFlow`, `Vercel`',
       'https://github.com/mayuras7685/KU-hackathon',
+      'Document Classifier',
       'HackThisFall 3.0',
       h3)
 
 with tab4:
-   txt('My first hackthon and started journey that never ends, to attending   such hackathon we can up to date with latest technology advancemnets, thier in hackthon we creted sort of feedback form that calculates the Happiness index of school students.',
+   txt("""DotSlash 6.0 By SVNIT, Surat. Out of 1200+ Registeration we select    for offline round. Our Project idea for this hackathon was Document Classifiacion system, I know you have a question why you recreacting same project? 
+       
+   Answer was Adding new Feature like KYC Document Classification
+        """,
       'Skills', 
       '`React`, `Tailwind CSS`, `Flask`, `OCR`, `TensorFlow`, `Vercel`',
       'https://github.com/mayuras7685/Unkils-DotSlash6.0-Submission',
+      'Document Classifier',
       'DotSlash 6.0',
       h4)
 
 with tab5:
-   txt('My first hackthon and started journey that never ends, to attending   such hackathon we can up to date with latest technology advancemnets, thier in hackthon we creted sort of feedback form that calculates the Happiness index of school students.',
+   txt("""participated in the Hack-Vengers Hackathon held at Parul University. It was a fantastic experience where we collaborated and created a Wild Animal detection project that we are proud of.
+
+We worked tirelessly to create a custom detection model that detects 6 classes (Lion, Tiger, Hyena, Fox, Wolf, Cheetah) and we are thrilled with the results (71% Accu.).
+
+*I know 71% is less but in 17 hours with no accurate dataset also to improve accuracy we try to train the model with different no. of epochs with different hyper-parameters but internet and colab not worked as well we wanted every time. (Colab's Runtime out or internet disconnects)""",
       'Skills', 
       '`Computer Vision`, `Yolov5`, `Roboflow`, `Colab`',
       'https://github.com/mayuras7685/Unkils_HackVengers',
+      'Wild Animal Detection',
       'Hackvengers',
       h5)
 
 with tab6:
-   txt('My first hackthon and started journey that never ends, to attending   such hackathon we can up to date with latest technology advancemnets, thier in hackthon we creted sort of feedback form that calculates the Happiness index of school students.',
+   txt("""attended a 36-hour hackathon called Wittyhacks organized by Datacode.in at NMIMS Indore. It was an incredible experience to be a part of this hackathon and showcase our skills and knowledge.
+
+During the hackathon, we worked hard and created a content recommendation system using neural collaborative filtering. We put in a lot of effort, worked late hours, and applied our expertise to develop a unique solution that could help tourists in selecting the right places to visit based on their interests.""",
       'Skills', 
       '`TensorFlow`, `React` , `Node.js`, `Mongodb`',
       'https://github.com/WittyhacksCR003/WH012_Unkils',
+      'Tourist Place Recommandtion System',
       'Wittyhacks 3.0',
       h6)
